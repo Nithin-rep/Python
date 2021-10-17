011faee957f25b8686309924caa11e98258b46ec
Nithin-rep <43278078+Nithin-rep@users.noreply.github.com>
2021-10-17 17:48:15 +0200

    Add files via upload

---
+++ string_basic_operations.py
@@ -0,0 +1,14 @@
+new_string = input("Enter a string: ")
+
+upper_case=new_string.upper()
+print(upper_case)
+
+Lower_case = new_string.lower()
+print(Lower_case)
+
+
+split1 = new_string.split("_")
+print(split1)
+
+split2 = new_string.split(" ")
+print(split2)
