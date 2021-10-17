9db1cffd39512f36dd4c33ae810031c87bf15f9d
Nithin-rep <43278078+Nithin-rep@users.noreply.github.com>
2021-10-17 22:47:04 +0600

    Update string_basic_operations.py

--- string_basic_operations.py
+++ string_basic_operations.py
@@ -1,17 +1,16 @@
 def method(new_string):
+    
     upper_case=new_string.upper()
     print(upper_case)
 
     Lower_case = new_string.lower()
     print(Lower_case)
 
-
     split1 = new_string.split("_")
     print(split1)
 
     split2 = new_string.split(" ")
     print(split2)
-
 
     print("indexing", new_string[::1])
 
