2a5fc905a4411b61a9cee0eed903685554c20ae0
Nithin-rep <43278078+Nithin-rep@users.noreply.github.com>
2021-10-17 22:46:40 +0600

    Update string_basic_operations.py

--- string_basic_operations.py
+++ string_basic_operations.py
@@ -1,14 +1,18 @@
-new_string = input("Enter a string: ")
+def method(new_string):
+    upper_case=new_string.upper()
+    print(upper_case)
 
-upper_case=new_string.upper()
-print(upper_case)
-
-Lower_case = new_string.lower()
-print(Lower_case)
+    Lower_case = new_string.lower()
+    print(Lower_case)
 
 
-split1 = new_string.split("_")
-print(split1)
+    split1 = new_string.split("_")
+    print(split1)
 
-split2 = new_string.split(" ")
-print(split2)
+    split2 = new_string.split(" ")
+    print(split2)
+
+
+    print("indexing", new_string[::1])
+
+method( input("Enter a string: "))
