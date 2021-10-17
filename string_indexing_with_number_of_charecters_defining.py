7ab85cb58c0d8bfa7f66ca80e6f26ffc5f7edc39
Nithin-rep <43278078+Nithin-rep@users.noreply.github.com>
2021-10-17 21:41:31 +0600

    Update string_indexing_with_number_of_charecters_defining.py

--- string_indexing_with_number_of_charecters_defining.py
+++ string_indexing_with_number_of_charecters_defining.py
@@ -1,8 +1,8 @@
-my_name = "I_AM_KNIGHT_KING"
-print(len(my_name))
+string = input("Enter the string you want to break: ")
+print("The string length is {} " .format(len(string)))
 
-breaker_at = int(input("enter the breaker point"))
-loop = int((len(my_name)/ breaker_at))
+breaker_at = int(input("Enter the number of charecters after which you want to break: "))
+loop = int((len(string)/ breaker_at))
 
 for i in range(0,loop):
-    print(my_name[(breaker_at * i) : (i* breaker_at + breaker_at)])
+    print(string[(breaker_at * i) : (i* breaker_at + breaker_at)])
